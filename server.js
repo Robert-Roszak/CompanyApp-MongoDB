030a41ddd7336f131122a7dc7e3ac6062e3d6fe7
Robert-Roszak <79662439+Robert-Roszak@users.noreply.github.com>
2021-11-12 17:31:34 +0100

    Add Mongoose

--- server.js
+++ server.js
@@ -1,52 +1,34 @@
 const express = require('express');
 const cors = require('cors');
-const mongoClient = require('mongodb').MongoClient;
+const mongoose = require('mongoose');
 
 const employeesRoutes = require('./routes/employees.routes');
 const departmentsRoutes = require('./routes/departments.routes');
 const productsRoutes = require('./routes/products.routes');
 
-mongoClient.connect('mongodb://localhost:27017/', { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
-  if(err) {
-    console.log(err);
-  }
-  else {
-    console.log('Successfully connected to the database');
-    const db = client.db('companyDB');
+const app = express();
 
-    /* db.collection('employees').find({ department: 'IT' }).toArray((err, data) => {
-      if(!err) console.log(data);
-    });
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-    db.collection('employees').findOne({ department: 'IT' }, (err, data) => {
-      if(!err) console.log(data);
-    });
+app.use('/api', employeesRoutes);
+app.use('/api', departmentsRoutes);
+app.use('/api', productsRoutes);
 
-    db.collection('departments').insertOne({ name: 'Management' }, err => {
-      if(err) console.log('err');
-    }); */
+app.use((req, res) => {
+  res.status(404).send({ message: 'Not found...' });
+})
 
-    const app = express();
+// connects our backend code with the database
+mongoose.connect('mongodb://localhost:27017/companyDB', { useNewUrlParser: true });
+const db = mongoose.connection;
 
-    app.use(cors());
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: false }));
-    // give all endpoints access to db
-    app.use((req, res, next) => {
-      req.db = db;
-      next();
-    });
+db.once('open', () => {
+  console.log('Connected to the database');
+});
+db.on('error', err => console.log('Error ' + err));
 
-    app.use('/api', employeesRoutes);
-    app.use('/api', departmentsRoutes);
-    app.use('/api', productsRoutes);
-
-    app.use((req, res) => {
-      res.status(404).send({ message: 'Not found...' });
-    })
-
-    app.listen('8000', () => {
-      console.log('Server is running on port: 8000');
-    });
-  }
+app.listen('8000', () => {
+  console.log('Server is running on port: 8000');
 });
